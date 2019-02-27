2633eea5eb78cf09882ad70a886974e64335a923
Schurk, Fabian <fabian.schurk@web.de>
2019-02-27 11:22:04 +0100

    sds

--- src/Engine.java
+++ src/Engine.java
@@ -13,6 +13,7 @@ public class Engine {
     }
     public void increaseRPM(int rpm){
         this.rpm +=rpm;
+        if(rpm>2550)rpm = 2550;
     }
     public void decreaseRPM(int rpm){
         this.rpm -=rpm;
