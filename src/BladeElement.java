7406533b2fe84ed9300c8b5ec9383fb2fd02a377
fabian.schurk@web.de <Fabi03071999>
2019-03-03 17:17:35 +0100

    läuft iwie fert

--- src/BladeElement.java
+++ src/BladeElement.java
@@ -4,8 +4,7 @@ public class BladeElement extends BladeElementUnit {
     public BladeElement(String name) {
         super(name);
         structure = new String[5];
-        for (int i = 0; i <= 4; i++) structure[i] = "++-/-*+*-*+:-*++";
-    }
+        for (int i = 0; i <= 4; i++) structure[i] = "++-/-*+*-*+:-*++";}
 
     public String[] getStructure() {
         return structure;
